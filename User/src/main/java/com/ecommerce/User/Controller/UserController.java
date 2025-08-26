a2ee644e9d4a26b5bd8722529b2d5334cd7e2bc4
kaif0705 <mohammedkaifkilledar5@gmail.com>
2025-08-26 18:59:44 +0530

    Implemented dedicated Logging mechanism for each Microservice

--- User/src/main/java/com/ecommerce/User/Controller/UserController.java
+++ User/src/main/java/com/ecommerce/User/Controller/UserController.java
@@ -5,6 +5,8 @@ import com.ecommerce.User.Payload.UserResponse;
 import com.ecommerce.User.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,14 +20,9 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-//    private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @GetMapping
-    public ResponseEntity<List<UserResponse>> getAllUsers(){
-        return new ResponseEntity<>(userService.fetchAllUsers(),
-                HttpStatus.OK);
-    }
-
+//    private static final Logger log= LoggerFactory.getLogger(UserController.class);
+    
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id){
         log.info("Request received for user: {}", id);
@@ -45,6 +42,12 @@ public class UserController {
     public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest){
         userService.addUser(userRequest);
         return ResponseEntity.ok("User added successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
+        return new ResponseEntity<>(userService.fetchAllUsers(),
+                HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
